eabd01b6ebe0d96bc05450842d3e6a89fa65923e
joriskalz <joris@kalz.de>
2020-10-08 16:23:12 +0200

    Shared Service

---
+++ STLFusionSimpleSample/Shared/Model/DataList.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STLFusionSimpleSample.Shared.Model
+{
+    public class DataList
+    {
+        public List<string> Messages { get; }
+
+        public DataList()
+        : this(new List<string>()) { }
+
+        [JsonConstructor]
+        public DataList(List<string> messages)
+        {
+            Messages = messages;
+        }
+    }
+}
---
+++ STLFusionSimpleSample/Shared/Services/IDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Stl.Fusion;
+
+namespace STLFusionSimpleSample.Shared.Services
+{
+    public interface IDataService
+    {
+        // Writers
+        Task<string> AddMessageAsync(string text, CancellationToken cancellationToken = default);
+
+        // Readers
+        [ComputeMethod(KeepAliveTime = 10)]
+        Task<DataList> GetDataListAsync(int length, CancellationToken cancellationToken = default);
+    }
+}
