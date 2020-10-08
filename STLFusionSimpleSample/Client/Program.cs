cec5d443f3a558e67df225bd122e150ee254cce6
joriskalz <joris@kalz.de>
2020-10-08 17:15:52 +0200

    Initial Client Services

--- STLFusionSimpleSample/Client/Program.cs
+++ STLFusionSimpleSample/Client/Program.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Stl.DependencyInjection;
+using Stl.Fusion;
+using Stl.Fusion.Blazor;
+using Stl.Fusion.Client;
+using Stl.OS;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +19,87 @@ namespace STLFusionSimpleSample.Client
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public const string ClientSideScope = nameof(ClientSideScope);
+
+        public static Task Main(string[] args)
         {
+            if (OSInfo.Kind != OSKind.WebAssembly)
+                throw new ApplicationException("This app runs only in browser.");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            ConfigureServices(builder.Services, builder);
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            var runTask = host.RunAsync();
+            Task.Run(async () =>
+            {
+                var hostedServices = host.Services.GetService<IEnumerable<IHostedService>>();
+                foreach (var hostedService in hostedServices)
+                    await hostedService.StartAsync(default);
+            });
+            return runTask;
+        }
+
+        public static void ConfigureServices(IServiceCollection services, WebAssemblyHostBuilder builder)
+        {
+            services.AddLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(LogLevel.Debug);
+                logging.AddDebug();
+            });
+
+            var baseUri = new Uri(builder.HostEnvironment.BaseAddress);
+            var apiBaseUri = new Uri($"{baseUri}api/");
+
+            var fusion = services.AddFusion();
+            var fusionClient = fusion.AddRestEaseClient(
+                (c, o) =>
+                {
+                    o.BaseUri = baseUri;
+                    o.MessageLogLevel = LogLevel.Information;
+                }).ConfigureHttpClientFactory(
+                (c, name, o) =>
+                {
+                    var isFusionClient = (name ?? "").StartsWith("Stl.Fusion");
+                    var clientBaseUri = isFusionClient ? baseUri : apiBaseUri;
+                    o.HttpClientActions.Add(client => client.BaseAddress = clientBaseUri);
+                });
+            var fusionAuth = fusion.AddAuthentication().AddClient().AddBlazor();
+
+            // This method registers services marked with any of ServiceAttributeBase descendants, including:
+            // [Service], [ComputeService], [RestEaseReplicaService], [LiveStateUpdater]
+
+            services.AttributeBased(ClientSideScope).AddServicesFrom(Assembly.GetExecutingAssembly());
+
+            //foreach (var item in services)
+            //{
+            //    if (item.ServiceType == typeof(IListExampleService) || item.ServiceType == typeof(IChatService))
+            //    {
+            //        Console.WriteLine(item?.ToString());
+            //    }
+
+            //}
+            ConfigureSharedServices(services);
+        }
+
+        public static void ConfigureSharedServices(IServiceCollection services)
+        {
+            // Default delay for update delayers
+            //services.AddSingleton(c => new UpdateDelayer.Options()
+            //{
+            //    Delay = TimeSpan.FromSeconds(0.1),
+            //});
+
+            //services.AddSingleton<IPluralize, Pluralizer>();
+
+            // This method registers services marked with any of ServiceAttributeBase descendants, including:
+            // [Service], [ComputeService], [RestEaseReplicaService], [LiveStateUpdater]
+            services.AttributeBased().AddServicesFrom(Assembly.GetExecutingAssembly());
         }
     }
 }
---
+++ STLFusionSimpleSample/Client/Services/Clients.cs
@@ -0,0 +1,26 @@
+﻿using RestEase;
+using Stl.Fusion.Client;
+using Stl.Fusion.Client.RestEase;
+using STLFusionSimpleSample.Shared.Model;
+using STLFusionSimpleSample.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace STLFusionSimpleSample.Client.Services
+{
+    [RestEaseReplicaService(typeof(IDataService), Scope = Program.ClientSideScope)]
+    [BasePath("data")]
+    public interface IDataClient : IRestEaseReplicaClient
+    {
+        // Writers
+        [Post("addMessage")]
+        Task<string> AddMessageAsync(long userId, string text, CancellationToken cancellationToken = default);
+
+        // Readers
+        [Get("getData")]
+        Task<DataList> GetDataListAsync(int length, CancellationToken cancellationToken = default);
+    }
+}
